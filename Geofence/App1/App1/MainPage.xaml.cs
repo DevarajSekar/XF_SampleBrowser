25871b19b4e9f16fa59b1a7f8a4a21d644d777eb
DevarajSekar <42164547+DevarajSekar@users.noreply.github.com>
2023-01-27 07:47:06 +0530

    Add files via upload
    Added geo fence code changes.

---
+++ Geofence/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,129 @@
+﻿using Rg.Plugins.Popup.Extensions;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public GeoClassCollection GeoClassCollection { get; set; }
+        int index { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            GeoClassCollection = new GeoClassCollection();
+            this.BindingContext = GeoClassCollection;
+        }
+
+        private void CarouselView_Scrolled(object sender, ItemsViewScrolledEventArgs e)
+        {
+            if (e.CenterItemIndex == 0)
+            {
+                index = 0;
+                this.Background = new LinearGradientBrush()
+                {
+                    EndPoint = new Point(0, 1),
+                    GradientStops = new GradientStopCollection()
+                {
+                    new GradientStop(){ Color= Color.FromHex("#5b1c63") , Offset=0.1f},
+                    new GradientStop(){ Color=Color.FromHex("#212a94" ), Offset=1.0f},
+                }
+                };
+            }
+            else
+            {
+                index = 1;
+                this.Background = new LinearGradientBrush()
+                {
+                    EndPoint = new Point(0, 1),
+                    GradientStops = new GradientStopCollection()
+                {
+                    new GradientStop(){ Color= Color.FromHex("#016eb9") , Offset=0.1f},
+                    //new GradientStop(){ Color=Color.FromHex("#686a5f") , Offset=0.75f},
+                    //new GradientStop(){ Color=Color.FromHex("#424539") , Offset=1.0f}
+                    new GradientStop(){ Color=Color.FromHex("#010c12") , Offset=0.8f},
+                    new GradientStop(){ Color=Color.FromHex("#010c12") , Offset=1.0f}
+                }
+                };
+            }
+        }
+
+        private async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            if (index == 0)
+            {
+                await Navigation.PushPopupAsync(new RgPage());
+            }
+            else if (index == 1)
+            {
+
+            }
+        }
+    }
+
+    public class GeoClass
+    {
+        public string templateImage { get; set; }
+        public string buttonImage { get; set; }
+        public string templateContent { get; set; }
+
+        public string templateTopic { get; set; }
+    }
+
+    public class GeoClassCollection : INotifyPropertyChanged
+    {
+        public ObservableCollection<GeoClass> geoClasses;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand TemplateButonClicked;
+
+        public GeoClassCollection()
+        {
+            GeoClasses = new ObservableCollection<GeoClass>()
+            {
+                new GeoClass(){
+                    templateTopic = "GEO-LOCATOR",
+                    templateImage="locator2.jpg",
+                    buttonImage="locatorbutton.png",
+                    templateContent="Geolocation makes it possible, from any device connected to the Internet, to obtain all types of information in real time and locate the user with pinpoint accuracy at a given point in time. Geolocation technology is the foundation for location-positioning services and location-aware applications (apps)."
+                },
+                new GeoClass(){
+                    templateTopic = "GEO-FENCING",
+                    templateImage="globe.jpg",
+                    buttonImage="fencubutton5.jpg",
+                    templateContent="Geofencing creates a virtual geographical boundary that triggers a marketing action to a mobile device when a user enters or exits that boundary.\n geofencing is focused on the virtual perimeter you build around a specific geographic location to deliver targeted messaging."
+                }
+            };
+        }
+
+        public ObservableCollection<GeoClass> GeoClasses
+        {
+            get
+            {
+                return geoClasses;
+            }
+            set
+            {
+                if (geoClasses != value)
+                {
+                    geoClasses = value;
+                    OnPropertyChanged(new PropertyChangedEventArgs("GeoClasses"));
+                }
+            }
+        }
+
+        private void OnPropertyChanged(PropertyChangedEventArgs eventArgs)
+        {
+            PropertyChanged?.Invoke(this, eventArgs);
+        }
+    }
+}
---
+++ Geofence/App1/App1/RgPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using Xamarin.Essentials;
+using System.ComponentModel;
+
+namespace App1
+{
+    public partial class RgPage : PopupPage
+    {
+        public RgPage()
+        {
+            InitializeComponent();
+            LoadAddress();
+        }
+
+        private async void LoadAddress()
+        {
+            try
+            {
+                double Latitude = 10.790483;
+                double Longitude = 78.704673;
+                var request = new GeolocationRequest(GeolocationAccuracy.Best);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                if (location != null)
+                {
+                    Latitude = location.Latitude;
+                    Longitude = location.Longitude;
+                }
+
+                var placemarks = await Geocoding.GetPlacemarksAsync(Latitude, Longitude);
+                var placemark = placemarks?.FirstOrDefault();
+                if (placemark != null)
+                {
+                    adminAreaLabel.Text = placemark.AdminArea;
+                    countryCodeLabel.Text = placemark.CountryCode;
+                    countryNameLabel.Text = placemark.CountryName;
+                    featureNameLabel.Text = placemark.FeatureName;
+                    localityNameLabel.Text = placemark.Locality;
+                    postalCodeNameLabel.Text = placemark.PostalCode;
+                    subAdminAreaNameLabel.Text = placemark.SubAdminArea;
+                    subThoroughfareNameLabel.Text = placemark.SubThoroughfare;
+                    thoroughfareNameLabel.Text = placemark.Thoroughfare;
+                    mainAreaNameLabel.Text = placemark.SubLocality;
+                }
+                customloader.IsRunning = false;
+                infoStack.IsVisible = true;
+            }
+            catch (FeatureNotEnabledException ex)
+            {
+                DisplayAlert("Feature Not Support", ex.Message, "OK");
+            }
+        }
+
+        private async void CloseBtn_Clicked(object sender, EventArgs e)
+        {
+            await PopupNavigation.Instance.PopAsync(true);
+        }
+    }
+}
