0759f5f905b639322a8eac57d9b837905b608f1f
DotNet Developer <uiuxexplorer@gmail.com>
2023-06-10 16:41:33 +0530

    Tap and order App

---
+++ TapFood/Geofence/App1/App1/FencePage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Rg.Plugins.Popup.Extensions;
+using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FencePage : PopupPage
+    {
+        public FencePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await PopupNavigation.Instance.PopAsync(true);
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            scroll.ScrollToAsync(0, 0, true);
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            scroll.ScrollToAsync(0, 25000, true);
+        }
+    }
+}
---
+++ TapFood/Geofence/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Rg.Plugins.Popup.Extensions;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushPopupAsync(new FencePage());
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().CloseMainWindow();
+        }
+    }
+}
